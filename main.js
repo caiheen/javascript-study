9633fe1d2e91a2bea7d30cfea3cf64e859fa96af
caiheen <gkdms0520@gmail.com>
2021-01-16 15:34:18 +0900

    Var ~ Data Type

---
+++ main.js
@@ -0,0 +1,38 @@
+// Data Type
+
+// // 원시타입
+// // number
+// const a = 5;
+// const b = 5.5;
+// const d = 3.14;
+
+// // string
+
+// const str1 = "123";
+// const str2 = "hello";
+
+// // boolean
+// true
+// false
+
+// // null
+// null 
+
+// // undefined
+
+// undefined
+
+const a = 5;
+const b = 5;
+
+console.log(a === b);
+
+// 참조타입
+
+const arr1 = [];
+const arr2 = arr1;
+
+arr1.push(1);
+
+console.log(arr1);
+console.log(arr2);
