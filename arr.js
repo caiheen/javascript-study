5add5142b63fa31db834995b6a476a036541e4c6
caiheen <gkdms0520@gmail.com>
2021-01-17 15:31:37 +0900

    Arr Method

--- arr.js
+++ arr.js
@@ -1,7 +1,8 @@
 // // 배열 ( Array )
 
-const arr = [1,2,3,4,5];
+const arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const arr2 = arr.slice(1, 4);
-
+const arr2 = arr.filter((item) => {
+    return 3 < item && item < 8;
+})
 console.log(arr2);
