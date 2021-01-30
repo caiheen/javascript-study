8f3db29bf0841ee515443353df1cff6dd8c0bc6b
caiheen <gkdms0520@gmail.com>
2021-01-30 11:10:28 +0900

    BMI

--- day05/main.js
+++ day05/main.js
@@ -1,19 +1,31 @@
-const input = document.querySelector("input");
-const gram = document.querySelector(".gram");
-const oz = document.querySelector(".oz");
-const pound = document.querySelector(".pound");
+const form = document.querySelector("form");
+const weight = document.querySelector(".weight");
+const height = document.querySelector(".height");
+const bmi = document.querySelector(".bmi");
+const message = document.querySelector('.message');
+function bmiCaculator(event){
+    event.preventDefault();
+    const weightVal =parseInt(weight.value);
+    const heightVal = parseInt(height.value) / 100;
 
-function weightConverter(event){
-    const value = parseInt(event.target.value);
-    console.log(value)
-    const gramValue = value * 1000;
-    const ozValue = value * 35.273962;
-    const poundValue = value * 2.20462262;
+    const result = weightVal / (heightVal * heightVal);
 
-    gram.innerHTML = gramValue;
-    oz.innerHTML = ozValue;
-    pound.innerHTML = poundValue;
+    bmi.innerHTML = result;
+
+    if(result <= 18.5){
+        message.innerText = "저체중 입니다.";
+        message.style.color = "red";
+    } else if(result > 18.5 && result <= 23 ){
+        message.innerText = "정상체중 입니다.";
+        message.style.color = "#222";
+    } else if(result > 23 && result <= 25){
+        message.innerText = "과체중 입니다.";
+        message.style.color = "green";
+    } else {
+        message.innerText = "고도비만입니다.";
+        message.style.color = "red";
+    }
 
 }
 
-input.addEventListener("change", weightConverter);
+form.addEventListener("submit", bmiCaculator);
