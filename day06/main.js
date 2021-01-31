446ad2227b847c64918666f26203475e1df4f061
caiheen <gkdms0520@gmail.com>
2021-01-31 15:26:54 +0900

    join(practice)

--- day06/main.js
+++ day06/main.js
@@ -2,14 +2,12 @@ const form = document.querySelector("form");
 const username = form.querySelector("#username");
 const email = form.querySelector("#email");
 const password = form.querySelector("#password");
-const confirmPassword = form.querySelector("#password2");
-
-console.log(form, username, email, password, confirmPassword);
+const cofirmpassword = form.querySelector("#password2");
 
 function checkEmpty(val){
     if(val === ""){
         return false;
-    }else{
+    } else {
         return true;
     }
 }
@@ -19,16 +17,16 @@ function join(e){
     const usernameVal = username.value;
     const emailVal = email.value;
     const passwordVal = password.value;
-    const confirmPasswordVal = confirmPassword.value;
-    if(!checkEmpty(usernameVal) || !checkEmpty(emailVal) || !checkEmpty(passwordVal) || !checkEmpty(confirmPasswordVal)){
-        alert("인풋에 값을 입력해 주세요");
-    } else if(passwordVal.length < 8 && passwordVal > 16){
-        alert("비밀번호 길이는 8~16자 사이입니다.");
-    } else if (passwordVal !== confirmPasswordVal){
-        alert("비밀번호가 서로 일치하지 않습니다.");
+    const cofirmpasswordVal = cofirmpassword.value;
+    if(!checkEmpty(usernameVal) || !checkEmpty(emailVal) || !checkEmpty(passwordVal) || !checkEmpty(cofirmpasswordVal)){
+        alert("비어있는 인풋에 입력해주세요");
+    } else if(passwordVal < 8 && passwordVal > 16){
+        alert("비밀번호는 8~16자리입니다");
+    } else if(passwordVal !== cofirmpasswordVal){
+        alert("비밀번호가 다릅니다");
     } else {
-        alert("가입이 완료되었습니다.");
+        alert("가입이 완료 되었습니다.");
     }
 }
 
-form.addEventListener('submit', join);
+form.addEventListener("submit", join);
