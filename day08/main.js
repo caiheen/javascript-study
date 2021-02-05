728a8da56b6f4d00526ba560a73703e85675366f
caiheen <gkdms0520@gmail.com>
2021-02-05 21:23:55 +0900

    class auto typing

--- day08/main.js
+++ day08/main.js
@@ -1,78 +1,58 @@
-// 객체 원시타입 문자열, 숫자, 불리언, null
-
-// Object, Array, Function
-
-// 배열이나 함수도 객체 취급을 한다.
-
-// 객체는 모두 함수로 만들어짐
-
-
-
-// function Car(wheels){ 
-//     this.wheels = wheels;
-// }
-
-// Car.prototype.run = function(){
-//     console.log("Running");
-// }
-
-// Car.prototype.breakes = function(){
-//     console.log("Break");
-// }
-
-// const hyundai = new Car("4");
-// const kia = new Car("2");
-
-// console.log(hyundai)
-// console.log(kia)
-// kia.run();
-// kia.breakes();
-
-// prototype object, prototpye link
-
-// function add(a, b){
-// //     return a + b;
-// // }
-
-// // const a = new add();
-// // add.prototype.name = "이름";
-// // add.prototype.minus = function(a, b){
-// //     return a - b;
-// // }
-// // console.log(add);
-// // console.log(a);
-// // console.log(a.minus(5, 4));
-
-class Car {
-    constructor(wheels, name){
-        this.wheels = wheels;
-        this.name = name
+class TypeWriter {
+    constructor(element, words, wait=3000){
+        this.element = element;
+        this.words = JSON.parse(words);
+        this.wait = parseInt(wait);
+        this.txt = "";
+        this.index = 0;
+        this.isDeleting = false;
     }
 
+    typing(){
+        const current = this.index % this.words.length;
+        const fullTxt = this.words[current];
+
+        if(this.isDeleting){
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.element.innerHTML = `<span>${this.txt}</span>`;
+
+        let speed = 300;
+
+        if(this.isDeleting){
+            speed /= 2;
+        }
+
+        if(this.txt === fullTxt && !this.isDeleting){
+            speed = this.wait;
+            this.isDeleting = true;
+        } else if(this.txt === "" && this.isDeleting){
+            this.isDeleting = false;
+            this.index++;
+            speed = 500;
+        }
+        setTimeout(() => this.typing(), speed);
+    }
+}
+
+class TpyeWriterV2 extends TypeWriter {
     test(){
         console.log("test");
     }
 }
 
-class Hyundai extends Car {
-   run(){
-       console.log("Run");
-   }
+function init(){
+    const span = document.querySelector(".type-txt");
+    const wordArray = span.getAttribute("data-words");
+    const time = span.getAttribute("data-wait");
 
-   nav(){
-       console.log("Navigator");
-   }
-
-   breaks(){
-        console.log("break");
-   }
+    const tpyingWriter = new TpyeWriterV2(span, wordArray, time);
+    console.log(tpyingWriter);
+    tpyingWriter.typing();
+    tpyingWriter.test();
 }
 
-const test = new Car("4", "test");
-const sonata = new Hyundai("4", "Sonata");
-console.log(test);
-console.log(sonata);
-test.test();
-
-sonata.test();
-sonata.run();
+init()
