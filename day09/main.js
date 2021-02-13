14ba69cda38e2c7340bc90b3327793d930bd49c6
caiheen <gkdms0520@gmail.com>
2021-02-13 14:46:51 +0900

    Timer

---
+++ day09/main.js
@@ -0,0 +1,49 @@
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const reset = document.querySelector(".reset");
+const hours = document.querySelector(".hours");
+const mins = document.querySelector(".mins");
+const secs = document.querySelector(".sec");
+
+let hour = 0;
+let min = 0;
+let sec = 0;
+let timer;
+
+function play(e){
+    timer = setTimeout(function(){
+        sec++;
+        if(sec > 59){
+            sec = 0;
+            min++;
+        }
+        if(min > 59){
+            min = 0;
+            hour++;
+        }
+        // console.log(sec);
+        hours.innerHTML = hour < 10 ? "0"+hour : hour;
+        mins.innerHTML = min < 10 ? "0"+min : min;
+        secs.innerHTML = sec < 10 ? "0"+sec : sec;
+        play();
+
+    }, 1000);
+}
+
+function pause(){
+    clearTimeout(timer);
+}
+
+function clear(){
+    hour = 0;
+    min = 0;
+    sec = 0;
+
+    hours.innerHTML = "00";
+    mins.innerHTML = "00";
+    secs.innerHTML = "00";
+}
+
+start.addEventListener('click', play);
+stop.addEventListener('click', pause);
+reset.addEventListener('click', clear);
