0293b232000114076a96f47f184865569566fdf1
caiheen <gkdms0520@gmail.com>
2021-02-20 13:24:42 +0900

    getmovies

--- day10/main.js
+++ day10/main.js
@@ -1,28 +1,32 @@
 const list = document.querySelector(".list");
-const button = document.querySelector(".paging");
-let number = 1;
-async function getMovie(){
-    const reponse = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-KR&page=${number}`);
-    const {results} = await reponse.json();
-    results.forEach(item => {
+const paging = document.querySelector(".paging");
+let num = 1;
+
+async function movies(){
+    const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=1d21809e38135e1a123a0e36f69395e2&language=en-US&page=${num}`); 
+    const {results} = await response.json();
+
+    results.forEach(data => {
         const li = document.createElement("li");
         const h2 = document.createElement("h2");
-        const p = document.createElement("p");
         const img = document.createElement("img");
-        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`
-        h2.innerText = item.original_title;
-        p.innerText = item.overview;
-        li.appendChild(img);
+        const p = document.createElement("p");
+        
+        h2.innerHTML = data.original_title;
+        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face${data.poster_path}`;
+        p.innerHTML = data.overview;
+
         li.appendChild(h2);
+        li.appendChild(img);
         li.appendChild(p);
         list.appendChild(li);
     })
-    console.log(results);
-}
+    
+}   
+movies();
 
-getMovie();
-button.addEventListener('click', function(){
+paging.addEventListener('click', () => {
     list.innerHTML = "";
-    number++;
-    getMovie()
+    num++;
+    movies();
 })
