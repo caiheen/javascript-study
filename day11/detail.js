2e0943c2ef9c8130084abf46ef154fd808a4935b
caiheen <gkdms0520@gmail.com>
2021-02-21 18:32:31 +0900

    movie detail

--- day11/detail.js
+++ day11/detail.js
@@ -3,13 +3,14 @@ const title = document.querySelector(".title");
 const overview = document.querySelector(".overview");
 
 async function getDetail(id){
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-KR`);
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-kR`);
     const data = await response.json();
     console.log(data);
-
-    wrap.style.backgroundImage = `url("https://www.themoviedb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,032541,01b4e4)${data.backdrop_path}")`;
+    
     title.innerHTML = data.title;
     overview.innerHTML = data.overview;
+    wrap.style.backgroundImage = `url("https://www.themoviedb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,032541,01b4e4)${data.backdrop_path}")`
+    wrap.style.backgroundSize = "cover";
 }
 const id = window.location.search.slice(4);
 getDetail(id);
--- day11/main.js
+++ day11/main.js
@@ -1,113 +1,98 @@
 const movieList = document.querySelectorAll(".movie-list");
 
-async function getPapular(){
+async function getpapular(){
     const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-KR&page=1`);
     const {results} = await response.json();
-    // console.log(results);
+
     results.forEach(item => {
         const li = document.createElement("li");
         const img = document.createElement("img");
         const h3 = document.createElement("h3");
-        const anchor = document.createElement("a");
         const span = document.createElement("span");
-
+        const anchor = document.createElement("a");
         li.classList.add("movie-card");
-        h3.classList.add("movie-title");
-        span.classList.add("upcoming-day");
-        h3.innerHTML = item.original_title;
-        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`;
+        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`;
+        h3.innerHTML = item.title;
         span.innerHTML = item.release_date;
         anchor.href = `detail.html?id=${item.id}`
+        li.appendChild(anchor);
         anchor.appendChild(img);
         anchor.appendChild(h3);
         anchor.appendChild(span);
-        li.appendChild(anchor);
         movieList[0].appendChild(li);
     });
+    console.log(results);
 }
-getPapular();
+getpapular();
 
-async function getNow(){
+async function getNowplaying(){
     const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-KR&page=1`);
     const {results} = await response.json();
-    // console.log(results);
 
     results.forEach(item => {
         const li = document.createElement("li");
-        const h3 = document.createElement("h3");
         const img = document.createElement("img");
+        const h3 = document.createElement("h3");
         const span = document.createElement("span");
         const anchor = document.createElement("a");
-
         li.classList.add("movie-card");
-        h3.classList.add("movie-title");
-        span.classList.add("upcoming-day");
-        anchor.href = `detail.html?id=${item.id}`;
-        h3.innerHTML = item.original_title;
-        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`;
+        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`;
+        h3.innerHTML = item.title;
         span.innerHTML = item.release_date;
-
+        anchor.href = `detail.html?id=${item.id}`
+        li.appendChild(anchor);
         anchor.appendChild(img);
         anchor.appendChild(h3);
         anchor.appendChild(span);
-        li.appendChild(anchor);
         movieList[1].appendChild(li);
     });
 }
-getNow();
+getNowplaying();
 
-async function upComing(){
-    const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-KR&page=1`)
+async function getupcoming(){
+    const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-KR&page=1`);
     const {results} = await response.json();
-    
 
     results.forEach(item => {
         const li = document.createElement("li");
+        const img = document.createElement("img");
         const h3 = document.createElement("h3");
         const span = document.createElement("span");
-        const img = document.createElement("img");
         const anchor = document.createElement("a");
-
         li.classList.add("movie-card");
-        h3.classList.add("movie-title");
-        span.classList.add("upcoming-day");
-        anchor.href = `detail.html?id=${item.id}`;
-        h3.innerHTML = item.original_title;
-        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`;
+        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`;
+        h3.innerHTML = item.title;
         span.innerHTML = item.release_date;
+        anchor.href = `detail.html?id=${item.id}`
         li.appendChild(anchor);
+        anchor.appendChild(img);
+        anchor.appendChild(h3);
+        anchor.appendChild(span);
         movieList[2].appendChild(li);
-        anchor.appendChild(img);
-        anchor.appendChild(h3);
-        anchor.appendChild(span);
-    });
-}   
-upComing();
-
-async function topRated(){
-    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-KR&page=1`)
-    const {results} = await response.json();
-    console.log(results);
-
-    results.forEach(item => {
-        const li = document.createElement("li");
-        const img = document.createElement("img");
-        const span = document.createElement("span");
-        const h3 = document.createElement("h3");
-        const anchor = document.createElement("a");
-
-        li.classList.add("movie-card");
-        h3.classList.add("movie-title");
-        span.classList.add("upcoming-day");
-        anchor.href = `detail.html?id=${item.id}`;
-        h3.innerHTML = item.original_title;
-        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`;
-        span.innerHTML = item.release_date;
-        anchor.appendChild(li);
-        movieList[3].appendChild(li);
-        anchor.appendChild(img);
-        anchor.appendChild(h3);
-        anchor.appendChild(span);
     });
 }
-topRated()
+getupcoming();
+
+async function topRated(){
+    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=1d21809e38135e1a123a0e36f69395e2&language=ko-KR&page=1`);
+    const {results} = await response.json();
+    results.forEach(item => {
+        const li = document.createElement("li");
+        const h3 = document.createElement("h3");
+        const span = document.createElement("span");
+        const img = document.createElement("img");
+        const anchor = document.createElement("a");
+
+        li.classList.add("movie-card");
+        h3.innerHTML = item.title;
+        span.innerHTML = item.release_date;
+        img.src = `https://www.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`;
+        anchor.href = `detail.html?id=${item.id}`;
+        anchor.appendChild(img);
+        anchor.appendChild(h3);
+        anchor.appendChild(span);
+        li.appendChild(anchor);
+        movieList[3].appendChild(li);
+    });
+}
+topRated();
