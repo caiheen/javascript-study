8723d0ba9081c8355219fa724cf635364055ce6f
caiheen <gkdms0520@gmail.com>
2021-03-01 21:58:45 +0900

    youtube

--- day12/main.js
+++ day12/main.js
@@ -5,29 +5,30 @@ const list = document.querySelector(".list");
 async function getData(term){
     const res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${term}&type=video&key=AIzaSyBebrEaOeinIk1yp5OVt_7vSxgJ4FYwEis`);
     const {items} = await res.json();
+    list.innerHTML = "";
     console.log(items);
     items.forEach((item) => {
         const {snippet} = item;
         const {thumbnails} = snippet;
         const h3 = document.createElement("h3");
+        const p = document.createElement("p");
         const img = document.createElement("img");
         const li = document.createElement("li");
-        const p = document.createElement("p");
+        img.src = thumbnails.default.url;
         h3.innerHTML = snippet.title;
         p.innerHTML = snippet.description;
-        img.src = thumbnails.default.url;
-        li.appendChild(h3);
         li.appendChild(img);
+        li.appendChild(h3);
         li.appendChild(p);
         list.appendChild(li);
     });
 }
 
-function searchData(e){
+function searchForm(e){
     e.preventDefault();
     const value = input.value;
-    getData(value)
-    input.value = ""
+    getData(value);
+    input.value = "";
 }
 
-search.addEventListener('submit', searchData)
+search.addEventListener('submit', searchForm);
