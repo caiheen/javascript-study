968d9ab22e07ac52ff571e7199d8d00a6b549601
caiheen <gkdms0520@gmail.com>
2021-03-20 15:11:43 +0900

    parallax image effect

--- day13/main.js
+++ day13/main.js
@@ -7,31 +7,33 @@ const layer6 = document.querySelector(".layer-6");
 const layer7 = document.querySelector(".layer-7");
 const layer8 = document.querySelector(".layer-8");
 
+// animation 삭제
 setTimeout(function(){
     layaPlease.forEach(elem => {
         elem.style.animation = "none";
-    })
-}, 1500);
+    });
+},1000);
 
+// mousemove 이벤트 함수
 document.body.addEventListener('mousemove', function(e){
-    if(e.currentTarget.dataset.triggered){
-        layaPlease.forEach((elem, index) => {
-            if(elem.getAttribute('style')){
-                // style이 있을때
-                elem.style.transition = "all 0.4s";
-                elem.style.transform = "none";
-            }
-        })
+    console.log(e);
+    // 트리거가 없을떄 트렌지션 추가, 트랜스폼 none
+    if(!e.currentTarget.dataset.triggered){
+        layaPlease.forEach(elem => {
+            elem.style.transition = "all 0.5s";
+            elem.style.transform = "none";
+        });
     }
-
+    // 트리거 true
     e.currentTarget.dataset.triggered = true;
+    // 트랜스폼
     let width = window.innerWidth / 2;
-    let mouseMove2 = ((width - e.pageX) / 50)
-    let mouseMove3 = ((width - e.pageX) / 40)
-    let mouseMove4 = ((width - e.pageX) / 30)
-    let mouseMove5 = ((width - e.pageX) / 20)
-    let mouseMove6 = ((width - e.pageX) / 10)
-    let mouseMove7 = ((width - e.pageX) / 5);
+    let mouseMove2 = (width - e.pageX) / 50;
+    let mouseMove3 = (width - e.pageX) / 40;
+    let mouseMove4 = (width - e.pageX) / 30;
+    let mouseMove5 = (width - e.pageX) / 20;
+    let mouseMove6 = (width - e.pageX) / 10;
+    let mouseMove7 = (width - e.pageX) / 5;
 
     layer3.style.transform = `translateX(${mouseMove2}px)`;
     layer4.style.transform = `translateX(${mouseMove3}px)`;
@@ -41,6 +43,8 @@ document.body.addEventListener('mousemove', function(e){
     layer8.style.transform = `translateX(${mouseMove7}px)`;
 });
 
+    
+
 document.body.addEventListener('mouseleave', function(){
     layaPlease.forEach(elem => {
         elem.style.transition = "all 0.4s";
@@ -49,7 +53,6 @@ document.body.addEventListener('mouseleave', function(){
 });
 
 // document.body.addEventListener('mouseenter', function(){
-//     console.log("test")
 //     layaPlease.forEach(elem => {
 //         setTimeout(function(){
 //             elem.style.transition = "none";
