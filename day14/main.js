cf047a0bbb97154b6f482f3c8b7406a7628df41a
caiheen <gkdms0520@gmail.com>
2021-03-21 16:06:21 +0900

    fullpage

--- day14/main.js
+++ day14/main.js
@@ -3,44 +3,43 @@ const SHOW = "show";
 let next;
 let prev;
 
-const scrollEffect = (elem) => {
+// scroll Effect
+function scrollEffect(elem){
     window.scrollTo({
-        behavior:'smooth',
-        left:0,
-        top: elem.offsetTop,
-    });
-};
+        'behavior':'smooth',
+        'left':0,
+        'top':elem.offsetTop
+    })
+}
 
-
-const scrolling = e => {
+// whell event handler
+function scrolling(e){
     let tags = ['HEADER', 'SECTION', 'FOOTER'];
-    const {target} = e;
-    // const target = e.target;
+    
+    const target = e.target;
     if(tags.includes(target.nodeName)){
         next = target.closest(tags).nextElementSibling;
         prev = target.closest(tags).previousElementSibling;
-    } 
+    }
 
-    console.log(e.deltaY)
     if(e.deltaY < 0){
         e.preventDefault();
-        const {target} = e;
         if(tags.includes(prev.nodeName)){
-            scrollEffect(prev, 300, 'easeInOutQuint');
+            scrollEffect(prev);
             target.closest(tags).classList.remove(SHOW);
             prev.classList.add(SHOW);
         }
     } else if(e.deltaY > 0){
         e.preventDefault();
-        const {target} = e;
         if(tags.includes(next.nodeName)){
-            scrollEffect(next, 300, 'easeInOutQuint');
+            scrollEffect(next);
             target.closest(tags).classList.remove(SHOW);
             next.classList.add(SHOW);
         }
     } else {
         return false;
     }
-}
+    console.log(e);
+};
 
 document.body.addEventListener('wheel', scrolling);
