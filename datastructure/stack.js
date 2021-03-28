3ed3831858f76e0581b1f80443539ec91717f486
caiheen <gkdms0520@gmail.com>
2021-03-28 13:02:46 +0900

    scss

--- datastructure/stack.js
+++ datastructure/stack.js
@@ -1,35 +1,23 @@
-// 자료구조
-// 배열 0,1,2,3,4
-// 스택 [0,1,2,3,4]
-// 큐
-
 class Stack {
     constructor(){
-        this.store = []
+        this.store = [];
     }
-
-    push(data) {
+    push(data){
         this.store.push(data);
     }
-
     pop(){
         return this.store.pop();
     }
-
     peek(){
         return this.store[this.store.length - 1];
     }
 }
 
-
-const stack = new Stack();
-console.log(stack);
-
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.push(4)
-stack.pop()
-stack.pop()
-const stackPeek = stack.peek();
-console.log(stack, stackPeek)
+const stack = new Stack()
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.pop();
+const stackpeek  = stack.peek();
+console.log(stack, stackpeek);
---
+++ scss/README.md
@@ -0,0 +1,17 @@
+# SCSS
+
+---
+
+- node sass
+- ruby sass
+
+- vscode plugin
+
+
+---
+
+## node-sass
+
+> node-sass [option] scss경로 css가생성될경로
+
+node-sass scss/style.scss dist/style.css
