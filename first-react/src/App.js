2ec14f78ab1c88edb42e883b97fe75edaeccc898
caiheen <gkdms0520@gmail.com>
2021-05-02 15:37:35 +0900

    react

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,30 +1,53 @@
 import React, {useState} from 'react';
-import Header from './Header';
 import Name from './Name';
-import Footer from './Footer';
-import Counter from './Counter';
+
 
 function App() {
+  const [user, setUser] = useState({
+    name: "",
+    age: "",
+    email: ""
+  });
+  // const [name, setName] = useState("");
+  // const [age, setAge] = useState("");
+  // const [email, setEmail] = useState("");
+  // function getName(e){
+  //   setName(e.target.value);
+  // }
+  // function getAge(e){
+  //   setAge(e.target.value);
+  // }
+  // function getEmail(e){
+  //   setEmail(e.target.value);
+  // }
 
-  const [age, setAge] = useState(0);
-
-  function addAge (){
-    setAge(age + 3);
+  function getUser(e){
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(user)
+    setUser({
+      // 유저 객체 복사
+      ...user,
+      [name]: value
+    })
+  }
+ 
+  function reset(){
+    setUser({
+      name: "",
+      age: "",
+      email: ""
+    })
   }
 
-  function print(){
-    console.log("Hello")
-  }
-  // login 이름 뽑아옴
   return (
     // jsx
     <>
-      <Header name="현규" age="12" />
-      <Name name="하은" age={age} />
-      <button onClick={print}>나이 추가</button>
-      <Footer />
-      
-      <Counter />
+      <input name="name" onChange={getUser} value={user.name} type="text"/>
+      <input name="age" onChange={getUser} value={user.age} type="text"/>
+      <input name="email" onChange={getUser} value={user.email} type="text"/>
+      {user.name ? (<Name name={user.name} age={user.age} email={user.email} />):null}
+      <button onClick={reset}>reset</button>
     </>
   );
 }
--- first-react/src/Name.js
+++ first-react/src/Name.js
@@ -5,6 +5,7 @@ function Name(props){
         <>
         <h1>{props.name}님 어서오세요.</h1>
         <h2>{props.age}</h2>
+        <h3>{props.email}</h3>
         </>
     )
 }
